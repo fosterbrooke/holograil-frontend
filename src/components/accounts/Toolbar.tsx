ec0c5a2df0f7aef4f02a962d310474fc1e655816
fosterbrooke <okisahandsome@gmail.com>
2024-12-06 04:55:28 -0600

    fix: deleted unused variable'

--- src/components/accounts/Toolbar.tsx
+++ src/components/accounts/Toolbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaRegCalendarMinus, FaSearch } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 import Avatar from './Avatar';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
