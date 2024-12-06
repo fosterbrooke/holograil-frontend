9f18b2913f62083fad3a59cd262ec6bfa8d15f19
fosterbrooke <okisahandsome@gmail.com>
2024-12-05 18:08:21 -0600

    feat: scroll to ChoosePlanSlide panel when click 'Get Started' button on the first page

--- src/components/RoundButton.tsx
+++ src/components/RoundButton.tsx
@@ -4,17 +4,20 @@ interface RoundButtonProps {
   text: string;
   isMain?: boolean;
   className?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 const RoundButton: React.FC<RoundButtonProps> = ({
   text,
   isMain = true,
   className = '',
+  onClick,
 }) => {
   return (
     <div>
       <button
         className={`sm:text-[16px] text-[11px] sm:leading-[19px] sm:leading-[13px] sm:py-[16px] font-semibold transform duration-200 hover:scale-105 px-[29px] border-2 border-primary ${isMain ? 'text-white bg-primary' : 'text-primary bg-white'} ${className} py-[11px]`}
+        onClick={onClick}
       >
         {text}
       </button>
