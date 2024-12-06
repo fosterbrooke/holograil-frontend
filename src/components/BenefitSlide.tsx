621afa31fcfe5c0961b92841517fe5b97356f1af
fosterbrooke <okisahandsome@gmail.com>
2024-12-06 05:53:57 -0600

    fix: navigate when click get start button

--- src/components/BenefitSlide.tsx
+++ src/components/BenefitSlide.tsx
@@ -2,6 +2,20 @@ import React from 'react';
 import RoundButton from './RoundButton';
 
 const BenefitSlide: React.FC = () => {
+  const handleGetStart = () => {
+    const target = document.getElementById('chooseplanslide');
+    const offsetX = 100;
+
+    if (target) {
+      const targetPosition = target.getBoundingClientRect().top + window.scrollY;
+
+      window.scrollTo({
+        top: targetPosition - offsetX,
+        behavior: 'smooth'
+      });
+    }
+  }
+
   return (
     <div className="flex justify-center mb-[61px] sm:mx-0 mx-[46px]">
       <div className="mt-[82px] flex flex-col space-y-[51px] sm:w-[67%] w-full">
@@ -17,7 +31,11 @@ const BenefitSlide: React.FC = () => {
             photo booth software to ensure every event is a success. Choose our
             software for reliability, innovation, and results that stand out.
           </div>
-          <RoundButton text="Get Started ->" className="mt-2 rounded-[15px] max-w-[180px]" />
+          <RoundButton 
+            text="Get Started ->" 
+            className="mt-2 rounded-[15px] max-w-[180px]" 
+            onClick={handleGetStart}
+          />
         </div>
         <img src="/benefit_slide1.png" className="my-[45px]" />
       </div>
